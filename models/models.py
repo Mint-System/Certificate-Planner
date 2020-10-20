6b5e3958d60b568cc6da42bcab282c29d970ffa4
Janik von Rotz <login@janikvonrotz.ch>
2020-10-20 12:05:06 +0200

    update models and add icon

--- models/models.py
+++ models/models.py
@@ -2,33 +2,35 @@
 
 from odoo import models, fields, api
 
+class revision(models.Model):
+    _name = 'certificate_planer.document.revision'
+    _description = 'certificate_planer.document.revision'
+
+    name = fields.Char(required=True, string="Title")
+    document_id = fields.Many2one("certificate_planer.document", "Document")
 
 class document(models.Model):
     _name = 'certificate_planer.document'
     _description = 'certificate_planer.document'
 
-    title = fields.Char(required=True)
-    part_ids = fields.Many2many('certificate_planer.part', string="Parts")
+    name = fields.Char(required=True, string="Title")
     description = fields.Text()
-
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    revision_ids = fields.One2many(
+        "certificate_planer.document.revision",
+        "document_id",
+        "Revisions",
+    )
+    part_ids = fields.Many2many('certificate_planer.part', string="Parts")
 
 class part(models.Model):
     _name = 'certificate_planer.part'
     _description = 'certificate_planer.part'
 
-    title = fields.Char(required=True)
-    bom_id = fields.Many2one('certificate_planer.part', string='BOM ID')
+    name = fields.Char(required=True, string="Title")
 
-    bom_ids = fields.One2many(
+    parent_id = fields.Many2one('certificate_planer.part', string='BOM ID')
+    child_ids = fields.One2many(
          'certificate_planer.part',
-         'bom_id',
+         'parent_id',
          string='BOM'
     )
