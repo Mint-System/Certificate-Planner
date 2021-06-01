679a9a2efb2030f123b8d76e72b4cc8f9955fbd5
Janik von Rotz <login@janikvonrotz.ch>
2021-06-01 15:42:35 +0200

    show part certificate with computed field

--- models/part.py
+++ models/part.py
@@ -12,10 +12,11 @@ class Part(models.Model):
     sequence = fields.Integer()
     designation = fields.Char(required=True)
     bom_ids = fields.Many2many("certificate_planer.bom", string="Parent BoMs")
-    bom_id = fields.Many2one("certificate_planer.bom", string="BoM", store=False, compute="_compute_get_bom_id")
     document_ids = fields.Many2many("certificate_planer.document", string="Documents")
     part_ids = fields.Many2many("certificate_planer.part", string="Child Parts", store=False, compute="_compute_get_part_ids")
-
+    # Inverted fiels without One2many
+    bom_id = fields.Many2one("certificate_planer.bom", string="BoM", store=False, compute="_compute_get_bom_id")
+    certificate_id = fields.Many2one("certificate_planer.certificate", store=False, compute="_compute_get_certificate_id")
 
     # constraints
     _sql_constraints = [
@@ -42,3 +43,6 @@ class Part(models.Model):
 
     def _compute_get_part_ids(self):
         self.part_ids = self.env['certificate_planer.bom'].search([('part_id','=',self.id)]).part_ids
+
+    def _compute_get_certificate_id(self):
+        self.certificate_id = self.env['certificate_planer.certificate'].search([('part_id','=',self.id)])
