7fad2be6803d0c069b677e25591c8cc0b3d1e222
Janik von Rotz <login@janikvonrotz.ch>
2021-08-11 16:44:06 +0200

    prereq count and change relation for bom fields

--- models/part.py
+++ models/part.py
@@ -1,5 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
+import logging
+_logger = logging.getLogger(__name__)
 
 class Part(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
@@ -12,6 +14,7 @@ class Part(models.Model):
     sequence = fields.Integer()
     designation = fields.Char(required=True)
     part_count = fields.Integer(compute='_compute_part_count')
+    prerequisite_count = fields.Integer(compute='_compute_prerequisite_count')
 
     bom_id = fields.Many2one("certificate_planer.bom", string="BoM", store=False, compute="_compute_get_bom_id")
     certificate_id = fields.Many2one("certificate_planer.certificate", store=False, compute="_compute_get_certificate_id")
@@ -20,9 +23,9 @@ class Part(models.Model):
     document_ids = fields.Many2many("certificate_planer.document", string="Documents", ondelete="restrict")
     change_ids = fields.Many2many("certificate_planer.change", string="Changes", ondelete="restrict")
     category_ids = fields.Many2many("certificate_planer.part_category", string="Categories", ondelete="restrict")
-    part_ids = fields.Many2many("certificate_planer.part", string="Child Parts", store=False, compute="_compute_get_part_ids")
-    part_ids = fields.Many2many("certificate_planer.part", string="Child Parts", store=False, compute="_compute_get_part_ids")
-
+    part_ids = fields.Many2many(related='bom_id.part_ids', string="Child Parts")
+    prerequisite_ids = fields.One2many(related='bom_id.prerequisite_ids', string="Prerequisites")
+    
     # constraints
     _sql_constraints = [
         ('name_unique', 'unique (name)', "Part with this Partnumber already exists."),
@@ -39,11 +42,12 @@ class Part(models.Model):
         for record in self:
             record.part_count = len(self.part_ids)
 
+    def _compute_prerequisite_count(self):
+        for record in self:
+            record.prerequisite_count = len(self.prerequisite_ids)
+
     def _compute_get_bom_id(self):
         self.bom_id = self.env['certificate_planer.bom'].search([('part_id','=',self.id)])
-
-    def _compute_get_part_ids(self):
-        self.part_ids = self.env['certificate_planer.bom'].search([('part_id','=',self.id)]).part_ids
 
     def _compute_get_certificate_id(self):
         self.certificate_id = self.env['certificate_planer.certificate'].search([('part_id','=',self.id)])
@@ -56,4 +60,16 @@ class Part(models.Model):
             "view_mode": "tree,form",
             "res_model": "certificate_planer.part",
             "domain": [("id", "in", [t.id for t in self.part_ids])],
+            "context": "{'create': False}",
+        }
+
+    def view_prerequisite_ids(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Prerequisites",
+            "view_mode": "tree,form",
+            "res_model": "certificate_planer.part",
+            "domain": [("id", "in", [t.child_id.id for t in self.prerequisite_ids])],
+            "context": "{'create': False}",
         }
