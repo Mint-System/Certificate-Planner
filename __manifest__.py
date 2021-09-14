26a453774e40996321cca546515cf0eab8e65196
Janik von Rotz <login@janikvonrotz.ch>
2021-09-14 17:22:31 +0200

    Bump version

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
     'website': "https://www.mint-system.ch",
 
     'category': 'Operations',
-    'version': '13.0.3.1.2',
+    'version': '13.0.3.2.0',
     'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
