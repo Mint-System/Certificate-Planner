e99e03cf45958ebdd23f8e40b16f9a6ee2c6439f
Janik von Rotz <login@janikvonrotz.ch>
2021-09-28 12:59:53 +0200

    change and grouped revisions

--- report/mdl_report.py
+++ report/mdl_report.py
@@ -1,6 +1,7 @@
 from odoo import api, models, _
 import logging
 _logger = logging.getLogger(__name__)
+import itertools
 
 class MDLReport(models.AbstractModel):
     _name = 'report.certificate_planer.mdl_report'
@@ -20,10 +21,27 @@ class MDLReport(models.AbstractModel):
         # Get revisions and filter by change > status > shown on report attribute
         revisions = document.revision_ids.filtered(lambda r: r.change_id.status_id.show_on_report == True or not r.change_id)
 
+        # Get changes from document > certificate
+        changes = document.certificate_id.change_ids
+        # Filter changes with status show on report
+        changes = changes.filtered(lambda r: r.status_id.show_on_report == True)
+
+        # Get documents by change
+        change_revisions = {}
+        for change in changes:
+            change_revisions[change.id] = {}
+            # Group document revisions by document > document type > document class
+            for key, items in itertools.groupby(change.revision_ids, lambda r: r.document_id.type_id.class_id.name):
+                change_revisions[change.id][key] = list(items)
+
+            _logger.info(change_revisions[change.id])
+
         return {
             'docs': documents,
             'current_revision': document.current_revision_id,
             'revisions': revisions,
+            'changes': changes,
+            'change_revisions': change_revisions,
             'title_page_text': self.env['ir.config_parameter'].sudo().get_param('certificate_planer.title_page_text'),
             'footer_text': self.env['ir.config_parameter'].sudo().get_param('certificate_planer.footer_text')
         }
