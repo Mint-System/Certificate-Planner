d599002c382c9d59e92d7e095713445baf1ca4b3
Janik von Rotz <login@janikvonrotz.ch>
2021-11-19 17:39:28 +0100

    hat sich geändert

--- patches/v13.0.3.5.1.sql
+++ patches/v13.0.3.5.1.sql
@@ -1,2 +1,6 @@
 CREATE SEQUENCE public.certificate_planer_document_revision_index_id_seq INCREMENT 1 START 200 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
 ALTER SEQUENCE public.certificate_planer_document_revision_index_id_seq OWNER TO odoo;
+
+CREATE SEQUENCE public.certificate_planer_change_classification_id_seq INCREMENT 1 START 10 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+ALTER SEQUENCE public.certificate_planer_change_classification_id_seq OWNER TO odoo;
+
