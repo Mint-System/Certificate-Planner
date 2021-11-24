20b2ed17b3081e4d0ef1bb976f50138e1d5d17a1
Janik von Rotz <login@janikvonrotz.ch>
2021-11-24 11:24:52 +0100

    rename is_legacy_change to hide_legacy_class

--- models/change_status.py
+++ models/change_status.py
@@ -14,4 +14,4 @@ class ChangeStatus(models.Model):
     sequence = fields.Integer()
     description = fields.Char()
     show_on_report = fields.Boolean("Show on MDL2 report")
-    is_legacy_change = fields.Boolean()
+    hide_legacy_class = fields.Boolean()
