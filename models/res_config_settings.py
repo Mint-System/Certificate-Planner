59bb093d1916e31bba33a9b0ee031809bf95c2cb
Janik von Rotz <login@janikvonrotz.ch>
2022-01-19 15:39:57 +0100

    new configuration tpi page index

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -4,6 +4,10 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
+    tpi_page_text = fields.Char(config_parameter='certificate_planer.tpi_page_text',
+                                default="This Index is approved with DOCUMENT_NAME DOCUMENT_REVISION_TITLE.",
+                                help="The placeholder DOCUMENT_NAME and DOCUMENT_REVISION_TITLE are populated when generating the report.")
+
     title_page_text = fields.Char(config_parameter='certificate_planer.title_page_text',
                                   default="This document contains confidential information and is proprietary to Aerolite AG. It may not be disclosed, reproduced or utilised in any way, in whole or in part, without the written consent of Aerolite AG.")
 
