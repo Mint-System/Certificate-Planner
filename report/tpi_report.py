08dd1850e0ebb1abfa9e61767f79f6a210aec3c2
Janik von Rotz <login@janikvonrotz.ch>
2022-01-24 11:55:20 +0100

    add document list to tpi report

--- report/tpi_report.py
+++ report/tpi_report.py
@@ -20,6 +20,46 @@ class TPIReport(models.AbstractModel):
         # Get first document
         document = documents[0]
 
+        # Get all documents and parts
+        documents_and_parts = []
+        documents_seen = []
+
+        def get_documents_by_parts(part_ids):
+            # Process each part
+            for part in part_ids:
+                # Get linked documents and process if not seen
+                for document in part.document_ids:
+                    # Append document if not seen
+                    if document.id not in documents_seen and document.type_id.class_id.show_on_report:
+                        documents_and_parts.append({
+                            'doc': document,
+                            'parts': document.part_ids
+                        })
+                        documents_seen.append(document.id)
+                # Run this function for subparts
+                get_documents_by_parts(
+                    part.bom_id.part_ids.certificate_planer_part_id)
+        get_documents_by_parts(document.certificate_id.part_id)
+        # Group by document classes
+        documents_by_class = {}
+        for document_class in self.env['certificate_planer.document_class'].search([]):
+            if document_class.show_on_report:
+                documents_by_class[document_class.name] = {}
+        for key, items in itertools.groupby(documents_and_parts, lambda r: r['doc'].type_id.class_id.name):
+            new_items = list(items)
+            # A key might be added multiple times
+            if documents_by_class.get(key):
+                items = documents_by_class[key]
+                items = items + new_items
+            else:
+                items = new_items
+            items = sorted(items, key=lambda r: r['doc'].name)
+            items = sorted(items, key=lambda r: r['doc'].type_id.sequence)
+            documents_by_class[key] = items
+        # Remove keys without docs
+        documents_by_class = dict(
+            filter(lambda r: r[1], documents_by_class.items()))
+
         # Replace placholders
         tpi_page_text = self.env['ir.config_parameter'].sudo().get_param('certificate_planer.tpi_page_text')
         tpi_page_text = tpi_page_text.replace('DOCUMENT_NAME', document.name).replace('DOCUMENT_REVISION_TITLE', document.current_revision_id.index_id.name)
@@ -27,6 +67,8 @@ class TPIReport(models.AbstractModel):
         return {
             'docs': documents,
             'current_revision': document.current_revision_id,
+            'print_date': document.print_date,
+            'documents_by_class': documents_by_class,
             'tpi_page_text': tpi_page_text,
             'title_page_text': self.env['ir.config_parameter'].sudo().get_param('certificate_planer.title_page_text'),
             'footer_text': self.env['ir.config_parameter'].sudo().get_param('certificate_planer.footer_text'),
