678a850a698aaf071ba0b9a977e7fbdd76163847
janikvonrotz <contact@janikvonrotz.ch>
2022-06-16 19:18:30 +0200

    [MIG][certificate_planer] migrate

--- certificate_planer/__manifest__.py
+++ certificate_planer/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Operations",
-    "version": "13.0.3.7.10",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["base", "mail"],
     "data": [
