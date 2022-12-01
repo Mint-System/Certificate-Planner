077e0e3a6940951fb13e40f092566fbad0d7ac80
janikvonrotz <contact@janikvonrotz.ch>
2022-12-01 10:08:07 +0100

    allow empty settings

--- certificate_planer/models/res_config_settings.py
+++ certificate_planer/models/res_config_settings.py
@@ -4,15 +4,10 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    tpi_page_text = fields.Char(config_parameter='certificate_planer.tpi_page_text',
-                                default="This Index is approved with DOCUMENT_NAME DOCUMENT_REVISION_TITLE.",
-                                help="The placeholder DOCUMENT_NAME and DOCUMENT_REVISION_TITLE are populated when generating the report.")
+    tpi_page_text = fields.Char(config_parameter='certificate_planer.tpi_page_text')
 
-    title_page_text = fields.Char(config_parameter='certificate_planer.title_page_text',
-                                  default="This document contains confidential information and is proprietary to Aerolite AG. It may not be disclosed, reproduced or utilised in any way, in whole or in part, without the written consent of Aerolite AG.")
+    title_page_text = fields.Char(config_parameter='certificate_planer.title_page_text')
 
-    footer_text = fields.Char(config_parameter='certificate_planer.footer_text',
-                              default="Copyright by Aerolite AG, Aumühlestrasse 10, CH-6373 Ennetbürgen")
+    footer_text = fields.Char(config_parameter='certificate_planer.footer_text')
 
-    design_organisation_statement_text = fields.Char(config_parameter='certificate_planer.design_organisation_statement_text',
-                                                     default="Approved under the authority of the Design Organisation No: EASA.21J.359")
+    design_organisation_statement_text = fields.Char(config_parameter='certificate_planer.design_organisation_statement_text')
