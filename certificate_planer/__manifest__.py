edf7c35d17fbd3cc459cb0ae08b2bc89681b52e3
Mitchel Admin <mitcheladmin@example.com>
2023-02-24 09:21:51 +0100

    new version

--- certificate_planer/__manifest__.py
+++ certificate_planer/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Operations",
-    "version": "14.0.1.8.0",
+    "version": "14.0.1.9.0",
     "license": "AGPL-3",
     "depends": ["base", "mail", "survey"],
     "data": [
