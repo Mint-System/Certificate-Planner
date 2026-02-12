95aedbcd29de81b5115ab40e79841afc3625a1aa
Michael Ziege <michael.ziege@juleg.ch>
2026-02-12 12:19:44 +0100

    feat(certificate_planner_export_pdm): export type configurable

--- certificate_planer_export_pdm/models/res_config_settings.py
+++ certificate_planer_export_pdm/models/res_config_settings.py
@@ -8,3 +8,8 @@ class ResConfigSettings(models.TransientModel):
     change_status_confirm_text = fields.Char(config_parameter='certificate_planer_export_pdm.change_status_confirm_text')
 
     pdm_export_dir = fields.Char(config_parameter='certificate_planer_export_pdm.pdm_export_dir')
+
+    pdm_export_type = fields.Selection(
+        config_parameter='certificate_planer_export_pdm.pdm_export_type',
+        selection=[('file', 'File export'), ('attachment', 'Attachment')],
+    )
--- certificate_planer_export_pdm/wizard/change_status_confirm.py
+++ certificate_planer_export_pdm/wizard/change_status_confirm.py
@@ -24,7 +24,13 @@ class ChangeStatusConfirm(models.TransientModel):
 
         change.write({"status_id": new_status_id})
 
-        # export as XML file
-        self.change_id._action_export_to_file()
+        export_type = self.env['ir.config_parameter'].sudo().get_param('certificate_planer_export_pdm.pdm_export_type')
+        if export_type == 'attachment':
+            # as attachment
+            self.change_id._action_export_to_attachment()
+        else:
+            # export as XML file
+            self.change_id._action_export_to_file()
+
         
         return {"type": "ir.actions.act_window_close"}
